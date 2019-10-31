9d60f15cb08719ca6db8cfcbc930ac65756ce0ae
Christian Graf <christian.graf@cyberfox.at>
2019-10-31 11:12:49 +0100

    feat(database/eloquent): Add trait to add a optional schema name property to a model.

---
+++ src/Database/Eloquent/Models/Concerns/DbSchema.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * ----------------------------------------------------------------------------
+ * This code is part of an application or library developed by Datamedrix and
+ * is subject to the provisions of your License Agreement with
+ * Datamedrix GmbH.
+ *
+ * @copyright (c) 2019 Datamedrix GmbH
+ * ----------------------------------------------------------------------------
+ * @author Christian Graf <c.graf@datamedrix.com>
+ */
+
+declare(strict_types=1);
+
+namespace DMX\Support\Database\Eloquent\Models\Concerns;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait DbSchema
+{
+    /**
+     * The database schema name associated with the model.
+     *
+     * @var string|null
+     */
+    protected $schema = null;
+
+    /**
+     * @return string|null
+     */
+    public function getSchemaName(): ?string
+    {
+        return !empty($this->schema) ? (string) $this->schema : null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTable()
+    {
+        $tableName = parent::getTable();
+        $schema = $this->getSchemaName();
+        if (!empty($schema) && $this->usingPostgreSQL()) {
+            return $schema . '.' . $tableName;
+        }
+
+        return $tableName;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getDatabaseDriverName(): string
+    {
+        return $this->getConnection()->getDriverName();
+    }
+
+    /**
+     * @param string $driverName
+     *
+     * @return bool
+     */
+    protected function usingDriver(string $driverName): bool
+    {
+        return strtolower($this->getDatabaseDriverName()) === strtolower($driverName);
+    }
+
+    /**
+     * @return bool
+     */
+    protected function usingMySQL(): bool
+    {
+        return $this->usingDriver('mysql');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function usingPostgreSQL(): bool
+    {
+        return $this->usingDriver('pgsql');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function usingMSSQL(): bool
+    {
+        return $this->usingDriver('sqlsrv');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function usingSqlLite(): bool
+    {
+        return $this->usingDriver('sqlite');
+    }
+}
